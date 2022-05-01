c504ec88f2656d30a2c6ba15ab5efadc51b9a31c
Shu(アクア) <92246611+Shu343@users.noreply.github.com>
2022-05-01 08:34:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@
 
 """ANIME DL TELEGRAM BOT CREDENTIALS"""
 
-api_id = ""
-api_hash = ""
-bot_token = ""
+api_id = "9124164"
+api_hash = "e922c979399634e27463ae08f06bc84a"
+bot_token = "5147947564:AAET9OIbJVoYCW0kfPO1vmSMUvou7QA2NK4"
